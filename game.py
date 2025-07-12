e8b85d4a5ad843391becb611e0a3062de5804596
toxie <gamepiatnitza@gmail.com>
2025-07-13 01:20:11 +0300

    refactor: enhance entity management with CardList and update public entity handling

--- game.py
+++ game.py
@@ -18,12 +18,19 @@ MAX_Y = GAME_FIELD_HEIGHT - 1
 PLAYER_SIDE_HEIGHT = 4
 PHARAOH_COORDINATES = (10, MAX_Y - 3)
 GUARD_COORDINATES = [(7, MAX_Y - 3), (13, MAX_Y - 3)]
+MAIN_WARRIOR_LIST_COORDINATES = (23, MAX_Y - 5)
+
+class deck(Enum):
+    WARRIOR = auto()
+    BANDAGE = auto()
+    BUILDING = auto()
 
 class PlayerManager():
     close_game = False
     player_num = 0
     my_turn = False
-    my_entities = {}
+    my_entities = {} # the structure of my_entities is as follows: my_entities[(x, y)] = Entity object
+    main_warrior_list = CardList(WarriorCard)
     received_entities = {}
     footer = ""
     second_player_joined = False
@@ -54,7 +61,13 @@ def refresh_screen():
     display_game_field()
 
 def send_public_entities():
-    public_entities = [entity_with_coords for entity_with_coords in PlayerManager.my_entities.items() if entity_with_coords[1].public]
+    public_entities = {}
+    for entity_with_coords in PlayerManager.my_entities.items():
+        if entity_with_coords[1].public:
+            if isinstance(entity_with_coords[1], CardList):
+                public_entities[entity_with_coords[0]] = entity_with_coords[1].get_public_cards()
+            else:
+                public_entities[entity_with_coords[0]] = entity_with_coords[1]
     sendall_with_end(s, SOCKET_SHARED_ENTITIES_UPDATE)
     sendall_with_end(s, pickle.dumps(public_entities))
 
@@ -62,7 +75,7 @@ def receive_public_entities():
     encoded_data = recvall(s)
     if encoded_data == SOCKET_SHARED_ENTITIES_UPDATE:
         received_entities = pickle.loads(recvall(s))
-        for entity_with_coords in received_entities:
+        for entity_with_coords in received_entities.items():
             PlayerManager.received_entities[(entity_with_coords[0][0], abs(entity_with_coords[0][1]-MAX_Y))] = entity_with_coords[1] # reverse y coordinate and add to the received entity list
         refresh_screen()
         return 1
@@ -74,9 +87,14 @@ def end_turn():
     sendall_with_end(s, SOCKET_YOUR_TURN)
     PlayerManager.my_turn = False
 
+def draw_a_card(from_deck, to_cardlist, public):
+    match from_deck:
+        case deck.WARRIOR:
+            to_cardlist.append(WarriorCard(face_values.NUM1, colors.GREEN, public))
+
 add_new_entity(Entity("╭" + "─" * (GAME_FIELD_WIDTH - 2) + "╮"), (MIN_X, MIN_Y))
-for i in range(1, GAME_FIELD_HEIGHT - 1):
-    add_new_entity(Entity("│"), [(MIN_X, i), (MAX_X, i)])
+for y in range(1, GAME_FIELD_HEIGHT - 1):
+    add_new_entity(Entity("│"), [(MIN_X, y), (MAX_X, y)])
 add_new_entity(Entity("╰" + "─" * (GAME_FIELD_WIDTH - 2) + "╯"), (MIN_X, MAX_Y))
 
 PlayerManager.footer = "Press spacebar when you are ready."
@@ -108,9 +126,12 @@ try:
     elif PlayerManager.player_num == 2:
         add_new_entity(Entity("-" * (GAME_FIELD_WIDTH - 2), colors.YELLOW), (1, MAX_Y - (PLAYER_SIDE_HEIGHT + 2)))
         add_new_entity(Entity("-" * (GAME_FIELD_WIDTH - 2), colors.BLUE), (1, PLAYER_SIDE_HEIGHT + 2))
-    add_new_entity(Entity(PHARAOH, colors.WHITE, public=True), PHARAOH_COORDINATES)
+    add_new_entity(Entity(face_values.PHARAOH, colors.WHITE, public=True), PHARAOH_COORDINATES)
     add_new_entity(GuardCard(), GUARD_COORDINATES[0])
     add_new_entity(GuardCard(), GUARD_COORDINATES[1])
+
+    add_new_entity(PlayerManager.main_warrior_list, MAIN_WARRIOR_LIST_COORDINATES)
+
     refresh_screen()
 
     send_public_entities()
@@ -121,9 +142,9 @@ try:
     while not PlayerManager.close_game:
         if PlayerManager.my_turn:
             keyboard.wait('space')
-            guard_cards_to_upgrade = [entity for coords, entity in PlayerManager.my_entities.items() if isinstance(entity, GuardCard) and entity.power != MAX_WARRIOR_CARD_POWER]
-            if guard_cards_to_upgrade:
-                random.choice(guard_cards_to_upgrade).upgrade_value()
+
+            draw_a_card(from_deck = deck.WARRIOR, to_cardlist = PlayerManager.main_warrior_list, public = True)
+
             refresh_screen()
             send_public_entities()
             end_turn()
--- shared_definitions.py
+++ shared_definitions.py
@@ -1,8 +1,11 @@
 import pickle
 import socket
 import threading
+import re
+from enum import Enum, auto
 
 class colors:
+    NONE = None
     GRAY = '\033[90m'
     WHITE = '\033[37m'
     RAINBOW = ''
@@ -12,31 +15,33 @@ class colors:
     RED = '\033[91m'
     ENDC = '\033[0m'
 
-PHARAOH = "↰"
-HOSPITAL = "HOS☩"
-SUPER_BARRACKS = "SUPⵌ"
-BARRACKS = "ⵌ"
-LEVEL_BANDAGE = "☛"
-FACE_VALUE_BANDAGE = "+"
-COMBINED_BANDAGE = "⇄"
-PLUS_2 = "+2"
-NUM1 = "1"
-NUM2 = "2"
-NUM3 = "3"
-NUM4 = "4"
-NUM5 = "5"
-NUM6 = "6"
-NUM7 = "7"
-NUM8 = "8"
-NUM9 = "9"
+class face_values:
+    PHARAOH = "↰"
+    HOSPITAL = "HOS☩"
+    SUPER_BARRACKS = "SUPⵌ"
+    BARRACKS = "ⵌ"
+    LEVEL_BANDAGE = "☛"
+    FACE_VALUE_BANDAGE = "+"
+    COMBINED_BANDAGE = "⇄"
+    PLUS_2 = "+2"
+    NUM1 = "1"
+    NUM2 = "2"
+    NUM3 = "3"
+    NUM4 = "4"
+    NUM5 = "5"
+    NUM6 = "6"
+    NUM7 = "7"
+    NUM8 = "8"
+    NUM9 = "9"
 
 main_colors = [colors.GREEN, colors.BLUE, colors.YELLOW, colors.RED]
-numerical_face_values = [NUM1, NUM2, NUM3, NUM4, NUM5, NUM6, NUM7, NUM8, NUM9, PLUS_2]
+numerical_face_values = [face_values.NUM1, face_values.NUM2, face_values.NUM3, face_values.NUM4, face_values.NUM5, face_values.NUM6, face_values.NUM7, face_values.NUM8, face_values.NUM9, face_values.PLUS_2]
 MAX_WARRIOR_CARD_POWER = len(main_colors) * len(numerical_face_values) - 1
 
 class Entity:
     def __init__(self, content, color = colors.GRAY, public = False):
-        self.content = content
+        if not hasattr(self, 'content'): # this is needed because in subclasses there may be a content property which is not settable. because of that an error may occur during super().__init__()
+            self.content = content
         self.color = color
         self.public = public
     def __repr__(self):
@@ -44,6 +49,8 @@ class Entity:
         if self.color == colors.RAINBOW:
             for i in range(0, len(self.content)):
                 return_str += (main_colors[i%4] + self.content[i] + colors.ENDC)
+        elif self.color == colors.NONE:
+            return_str = self.content
         else:
             return_str = self.color + self.content + colors.ENDC
 
@@ -53,6 +60,7 @@ class Entity:
         self.public = not self.public
 
 class WarriorCard(Entity):
+    type_name = "Warriors"
     __id_counter = 0
     def __init__(self, face_value, level, public = False):
         super(WarriorCard, self).__init__(face_value, level, public)
@@ -79,9 +87,53 @@ class WarriorCard(Entity):
         return self.__refresh_string_and_color()
 
 class GuardCard(WarriorCard):
+    type_name = "Guards"
     __id_counter = 0
-    def __init__(self, face_value = NUM1, level = colors.GREEN, public = True):
+    def __init__(self, face_value = face_values.NUM1, level = colors.GREEN, public = True):
         super(GuardCard, self).__init__(face_value, level, public)
+
+def remove_color_codes(str):
+    return re.compile(r'\x1B\[[0-?]*[ -/]*[@-~]').sub('', str)
+
+class CardList(Entity):
+    def __init__(self, card_type, cards=None, public=True):
+        self.card_type = card_type
+        self.__cards = cards if cards is not None else []
+        super(CardList, self).__init__(self.content, colors.NONE, public)
+    
+    def __repr__(self):
+        string_repr = self.card_type.type_name + ":"
+        if self.__cards:
+            for card in self.__cards:
+                string_repr += " " + repr(card)
+        else:
+            string_repr += " ***"
+            
+        return string_repr
+    
+    @property
+    def content(self):
+        return remove_color_codes(repr(self))
+    
+    def get_public_cards(self):
+        public_cards = [public_card for public_card in self.__cards if public_card.public]
+        return CardList(self.card_type, public_cards, public = True)
+    
+    def remove(self, card):
+        self.__cards.remove(card)
+        self.content = repr(self)
+    
+    def append(self, card):
+        self.__cards.append(card)
+    
+    def __setitem__(self, index, card):
+        self.__cards[index] = card
+
+    def __getitem__(self, index):
+        return self.__cards[index]
+    
+    def __len__(self):
+        return len(self.content)
 
 SOCKET_END_MSG = b"<END>"
 SOCKET_CONNECTION_ESTABLISHED = b"CONNECTION ESTABLISHED"
